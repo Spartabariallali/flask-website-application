8e978517dbf0667c10ef61f8aea4e2587afe87db
MOH <iAllali@Baris-macbook-pro.local>
2020-07-24 15:41:23 +0100

    added login attempts

--- flask-web-app/app.py
+++ flask-web-app/app.py
@@ -1,27 +1,44 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask,render_template,url_for,request,session,logging,redirect,flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, abort
+from functools import wraps
+
 
 
 app = Flask(__name__)
 
-@app.route("/", methods =['GET','POST'])
-
-def login_user():
-
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = "Invalid credentials. PLease try again."
-            return redirect(url_for('login_error '))
-
-        else:
-            return redirect(url_for('home_page'))
-    return render_template('login.html', error=error)
+app.secret_key = 'mysecretkey'
 
 @app.route("/home")
 
 def home_page():
+    session['attempt'] = 1
     return render_template("index.html")
+
+
+@app.route("/", methods =['GET','POST'])
+
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            attempt = int(session.get('attempt'))
+            if attempt == 2:
+                flash("This is your last chance")
+                # attempt += 1
+                # session['attempt'] = attempt
+            if attempt == 3:
+                flash('You have been logged out.')
+                return render_template("login_error_message.html")
+            else:
+                attempt += 1
+                session['attempt'] = attempt
+                error = 'Invalid Credentials. Please try again'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in.')
+            return redirect(url_for('home_page'))
+    return render_template('login.html', error=error)
+
+
 
 @app.route("/login_error")
 
